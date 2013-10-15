8adb4a9ec95ade5c2e8931867b6bbafeab540085
Diego Lago González <diego.lago.gonzalez@gmail.com>
2013-10-15 08:59:50 +0200

    New functions and refactorizations.
    New function to calculate CPU load.
    Refactorization of the function getting CPU idle time.

--- cpuload.cpp
+++ cpuload.cpp
@@ -1,8 +1,14 @@
+#include <stdexcept>
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <cstdlib>
+#include <ctime>
+
+#include <sys/time.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -10,7 +16,23 @@ const string CpuLoadFile	= "/proc/stat";
 const string EnvCpuLoadHS	= "CPULOAD_HS";
 const string EnvCpuLoadIdle	= "CPULOAD_IDLE";
 
-bool
+static long
+string_to_int(const string value) {
+	static stringstream s;
+	s.str(value);
+	long result;
+	s >> result;
+	return result;
+}
+
+static long
+time_hs() {
+	timeval t;
+	gettimeofday(&t, NULL);
+	return t.tv_sec * 100 + static_cast<long>(t.tv_usec / 10000.0);
+}
+
+static bool
 set_cpuload_env(const string name, int value) {
 	static stringstream var_ctor;
 
@@ -21,29 +43,49 @@ set_cpuload_env(const string name, int value) {
 	return true;
 }
 
-int
+static int
 get_cpuload_env(const string name, int new_value = 0) {
 	char* value = getenv(name.c_str());
-	if(value != NULL) {
-		
-		set_cpuload_env(name, new_value);
+	set_cpuload_env(name, new_value);
+	if(value != NULL) {	
+		return string_to_int(string(value));
 	} else {
-		set_cpuload_env(name, new_value);
 		return 0;
 	}
+}
+
+static float
+calculate_cpu_load(int hs, int idle, int last_hs, int last_idle) {
+	return 42;
+}
+
+static long
+get_cpu_idle(int processor_index = -1) {
+	string dummy, idle;
+
+	if(processor_index > -1) {
+		throw new runtime_error("CPU Load: Getting CPU load of any specific processor is unsupported.");
+	} else {
+		ifstream file(CpuLoadFile.c_str(), std::ifstream::in);
+		char* line = new char[128];
+		file.getline(line, 128);
+		istringstream sline(line);
+		delete[] line;	// Consumes a lot of time; should be commented since program ends ahead.
+
+		sline >> dummy >> dummy >> dummy >> dummy >> idle;	// Get forth value: cpu idle
+	}
+
+	return string_to_int(idle);
 }
 
 int
 main() {
 
-	ifstream file(CpuLoadFile.c_str(), std::ifstream::in);
-	char* line = new char[128];
-	file.getline(line, 128);
-	istringstream sline(line);
-	delete[] line;	// Consumes a lot of time; should be commented since program ends ahead.
+	long idle = get_cpu_idle();	
 
-	string dummy, idle;
-	sline >> dummy >> dummy >> dummy >> dummy >> idle;	// Get forth value: cpu idle
+	float cpu_load = calculate_cpu_load(time_hs(), idle, 0, 0);
+
+	cout << setw(2) << setfill('0') << setprecision(2) << fixed << cpu_load << endl;
 
 	return 0;
 }
